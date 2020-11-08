28440f52dac079c72e3b451eedc8bd7d34805844
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2020-11-08 13:01:29 -0800

    lambda buttons, lambda methods ip

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,11 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +22,9 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener(e -> add2Numbers());
+		randNumber.addActionListener(e -> randomNumber());
+		tellAJoke.addActionListener(e -> joke());
 		
 		window.setVisible(true);
 		window.pack();
@@ -27,6 +32,26 @@ public class LambdaButtons {
 	
 	public static void main(String[] args) {
 		new LambdaButtons();
+	}
+	
+	public static void add2Numbers() {
+		String m = JOptionPane.showInputDialog("pick a number");
+		int x = Integer.parseInt(m);
+		String m2 = JOptionPane.showInputDialog("pick another number");
+		int y = Integer.parseInt(m2);
+		JOptionPane.showMessageDialog(null, x+y);
+	}
+	
+	public static void randomNumber() {
+		Random rand = new Random();
+		int r = rand.nextInt(1000);
+		JOptionPane.showMessageDialog(null, r);
+	}
+	
+	public static void joke() {
+		JOptionPane.showInputDialog("Knock knock");
+		JOptionPane.showInputDialog("Tank");
+		JOptionPane.showMessageDialog(null, "You're welcome!");
 	}
 	
 	
