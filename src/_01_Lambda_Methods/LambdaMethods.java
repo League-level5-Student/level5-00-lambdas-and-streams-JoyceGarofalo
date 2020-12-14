8039c4566f2ef2281346373da9df2a89cc798f00
joycegarofalo <joycegarofalo@joyces-macbook.lan>
2020-12-13 17:58:19 -0800

    lambda methods, creating streams ip

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,17 +13,45 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		String reverse = "";
 		printCustomMessage((s)->{
-			for (int i = s.length(); i > 0; i--) {
-				//charAt(i);
+			String backwards = "";
+			for (int i = s.length()-1; i > -1; i--) {
+				backwards = backwards + s.charAt(i);
 			}
+			System.out.println(backwards);
 		}, "repeat");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			String caps = s.toUpperCase();
+			String low = s.toLowerCase();
+			for (int i = 0; i < s.length(); i++) {
+				s = s.replace(s.charAt(i), caps.charAt(i));
+			}
+			System.out.println("s");
+		}, "repeat");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			String n = ".";
+			String mix = "";
+			for (int i = 0; i < s.length(); i++) {
+				mix = mix + s.charAt(i) + n;
+			}
+			System.out.println(mix);
+		}, "repeat");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
+		printCustomMessage((s)->{
+			String vowels = "aeiou";
+			for (int i = 0; i < s.length(); i++) {
+				for (int x = 0; x < vowels.length(); x++) {
+					if(s.charAt(i) == vowels.charAt(x)) {
+						s = s.replaceAll(s.charAt(i) + "", "");
+					}
+				}
+				
+			}
+			
+			System.out.println(s);
+		}, "repeat");
 	
 	}
 	
